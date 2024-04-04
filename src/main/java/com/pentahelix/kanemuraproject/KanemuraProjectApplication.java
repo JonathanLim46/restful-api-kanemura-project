d92e7a019ecb7eeb1d2322946527d9029bc20112
JonathanLim46 <jonathanfrizy12@gmail.com>
2024-04-04 15:20:06 +0700

    spring boot first commit

---
+++ src/main/java/com/pentahelix/kanemuraproject/KanemuraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pentahelix.kanemuraproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanemuraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanemuraProjectApplication.class, args);
+	}
+
+}
